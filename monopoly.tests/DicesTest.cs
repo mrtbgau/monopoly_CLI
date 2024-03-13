5b0b263fc501b65ba3141745fb8c3970e0ea2a6e
mrtbgau <mrt.bgau@gmail.com>
2024-03-13 19:10:52 +0100

    Inplémentation TestRoll dices

---
+++ monopoly.tests/DicesTest.cs
@@ -0,0 +1,20 @@
+﻿using monopoly.classes;
+
+namespace monopoly.tests
+{
+    public class DicesTest
+    {
+        [Fact]
+        public void TestRoll()
+        {
+            Dices dices = new ();
+            int nb = dices.Roll();
+
+            Assert.True(nb.GetType() == typeof(int) );
+
+            Assert.True(nb >= 2 && nb <= 12);
+
+            Assert.True(nb == dices.result);
+        }
+    }
+}
